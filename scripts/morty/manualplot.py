bdc88c324024a9d88c072588b84b41f485bb6acf
Luke Seifert <LukeSeifert23@gmail.com>
2024-10-21 16:30:54 -0500

    update plotting (still in progress)

---
+++ scripts/morty/manualplot.py
@@ -0,0 +1,297 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+class PlotHolder:
+
+    def __init__(self):
+        self.data = {}
+        self.times = {}
+        self.msre = 'MSRE Experimental Data'
+        self.adder = 'MCNP/ADDER Data'
+
+        self.data[self.msre] = {}
+        self.data[self.adder] = {}
+        return
+    
+    def collect_data(self, cur_data=None, label=''):
+        self._add_MSRE_data()
+        self._add_ADDER_data()
+        self._add_cur_data(cur_data, label)
+        return
+    
+    def plot_data(self):
+        #self._setup_print()
+        nucs = self.data[self.msre].keys()
+        colors = ['blue', 'orange']
+        other_count = 0
+        for nuc in nucs:
+            for version in self.data.keys():
+                if version == self.msre:
+                    marker = '.'
+                    linestyle = ''
+                    color = 'black'
+                elif version == self.adder:
+                    marker = ''
+                    linestyle = '-.'
+                    color = 'red'
+                else:
+                    marker = ''
+                    linestyle = '--'
+                    color = colors[other_count]
+                    other_count += 1
+
+                plt.plot(np.asarray(self.times[version])/(24*3600),
+                         self.data[version][nuc], label=version,
+                         linestyle=linestyle, marker=marker, color=color,
+                         markersize=5)
+            plt.xlabel('Time [d]')
+            plt.ylabel('Concentration [atoms/cc]')
+            plt.legend()
+            plt.savefig(f'test_{nuc}.png')
+            plt.close()
+
+    
+    def _setup_print(self):
+        plt.rcParams["font.size"] = 16
+        plt.rcParams["axes.labelsize"] = 20
+        plt.rcParams["axes.labelweight"] = "bold"
+        plt.rcParams["lines.linewidth"] = 1.5
+        plt.rcParams["lines.markersize"] = 1
+        plt.rcParams["axes.grid"] = True
+        plt.rcParams["axes.grid.which"] = "major"
+        plt.rcParams["grid.linestyle"] = "--"
+        plt.rcParams["grid.linewidth"] = 1
+        plt.rcParams["xtick.direction"] = "in"
+        plt.rcParams["ytick.direction"] = "in"
+        plt.rcParams["xtick.major.size"] = 6.0
+        plt.rcParams["ytick.major.size"] = 6.0
+        plt.rcParams["xtick.minor.size"] = 3.0
+        plt.rcParams["ytick.minor.size"] = 3.0
+        plt.rcParams["figure.autolayout"] = True
+        plt.rcParams['savefig.dpi'] = 600
+        return
+
+    
+    def _add_MSRE_data(self):
+        self.times[self.msre] = [
+        1.024560E+07,
+        1.036800E+07,
+        1.317600E+07,
+        1.391040E+07,
+        1.441440E+07,
+        1.555200E+07,
+        2.229120E+07]
+
+        self.data[self.msre]['Zr95'] = [
+        np.nan,
+        np.nan,
+        np.nan,
+        np.nan,
+        2.04176E+16,
+        1.85615E+16,
+        1.67053E+16
+        ]
+
+        return
+    
+    def _add_ADDER_data(self):
+        adder_times = [
+        0.00E+00,
+        4.17E-01,
+        4.17E-01,
+        1.00E+00,
+        1.00E+00,
+        1.50E+00,
+        1.50E+00,
+        2.00E+00,
+        2.00E+00,
+        3.17E+00,
+        3.17E+00,
+        7.70E+01,
+        7.70E+01,
+        8.00E+01,
+        8.00E+01,
+        8.10E+01,
+        8.10E+01,
+        8.23E+01,
+        8.23E+01,
+        8.40E+01,
+        8.40E+01,
+        8.60E+01,
+        8.60E+01,
+        8.70E+01,
+        8.70E+01,
+        8.75E+01,
+        8.75E+01,
+        8.80E+01,
+        8.80E+01,
+        8.85E+01,
+        8.85E+01,
+        9.10E+01,
+        9.10E+01,
+        9.50E+01,
+        9.50E+01,
+        1.05E+02,
+        1.05E+02,
+        1.08E+02,
+        1.08E+02,
+        1.09E+02,
+        1.09E+02,
+        1.12E+02,
+        1.12E+02,
+        1.14E+02,
+        1.14E+02,
+        1.15E+02,
+        1.15E+02,
+        1.17E+02,
+        1.17E+02,
+        1.23E+02,
+        1.23E+02,
+        1.40E+02,
+        1.40E+02,
+        1.41E+02,
+        1.41E+02,
+        1.42E+02,
+        1.42E+02,
+        1.54E+02,
+        1.54E+02,
+        1.58E+02,
+        1.58E+02,
+        1.64E+02,
+        1.64E+02,
+        1.71E+02,
+        1.71E+02,
+        1.72E+02,
+        1.72E+02,
+        1.74E+02,
+        1.74E+02,
+        2.62E+02,
+        2.62E+02,
+        2.65E+02,
+        2.65E+02,
+        2.65E+02,
+        2.65E+02,
+        2.80E+02,
+        2.80E+02,
+        2.88E+02,
+        2.88E+02,
+        2.93E+02,
+        2.93E+02,
+        2.94E+02,
+        2.94E+02,
+        2.95E+02,
+        2.95E+02,
+        2.96E+02,
+        2.96E+02,
+        2.97E+02,
+        2.97E+02,
+        3.25E+02,
+        3.25E+02,
+        3.39E+02,
+        3.39E+02]
+
+        self.times[self.adder] = np.asarray(adder_times) * 24 * 3600
+
+        self.data[self.adder]['Zr95'] = [
+        0.00E+00,
+        8.70E+12,
+        8.70E+12,
+        8.86E+12,
+        8.86E+12,
+        2.98E+13,
+        2.98E+13,
+        3.00E+13,
+        3.00E+13,
+        1.28E+14,
+        1.28E+14,
+        5.81E+13,
+        5.81E+13,
+        3.09E+14,
+        3.09E+14,
+        3.06E+14,
+        3.06E+14,
+        5.66E+14,
+        5.66E+14,
+        5.58E+14,
+        5.58E+14,
+        9.67E+14,
+        9.67E+14,
+        9.59E+14,
+        9.59E+14,
+        1.16E+15,
+        1.16E+15,
+        1.16E+15,
+        1.16E+15,
+        1.36E+15,
+        1.36E+15,
+        1.33E+15,
+        1.33E+15,
+        2.95E+15,
+        2.95E+15,
+        2.65E+15,
+        2.65E+15,
+        3.90E+15,
+        3.90E+15,
+        3.89E+15,
+        3.89E+15,
+        5.65E+15,
+        5.65E+15,
+        5.55E+15,
+        5.55E+15,
+        6.00E+15,
+        6.00E+15,
+        5.90E+15,
+        5.90E+15,
+        8.70E+15,
+        8.70E+15,
+        7.20E+15,
+        7.20E+15,
+        7.74E+15,
+        7.74E+15,
+        7.71E+15,
+        7.71E+15,
+        1.38E+16,
+        1.38E+16,
+        1.32E+16,
+        1.32E+16,
+        1.59E+16,
+        1.59E+16,
+        1.89E+16,
+        1.89E+16,
+        1.87E+16,
+        1.87E+16,
+        1.94E+16,
+        1.94E+16,
+        7.47E+15,
+        7.47E+15,
+        8.45E+15,
+        8.45E+15,
+        8.43E+15,
+        8.43E+15,
+        1.42E+16,
+        1.42E+16,
+        1.30E+16,
+        1.30E+16,
+        1.52E+16,
+        1.52E+16,
+        1.50E+16,
+        1.50E+16,
+        1.55E+16,
+        1.55E+16,
+        1.53E+16,
+        1.53E+16,
+        1.58E+16,
+        1.58E+16,
+        1.17E+16,
+        1.17E+16,
+        1.71E+16,
+        1.71E+16
+        ]
+
+    def _add_cur_data(self, cur_data, label):
+        self.data[label] = {}
+        self.times[label] = cur_data['times']
+        for nuc in cur_data['data'].keys():
+            self.data[label][nuc] = cur_data['data'][nuc]
+        return
+        
--- scripts/morty/plotter.py
+++ scripts/morty/plotter.py
@@ -3,6 +3,9 @@ import os.path
 import os
 import numpy as np
 from time import time
+import pandas as pd
+from analysis import AnalysisCollection
+from manualplot import PlotHolder
 
 class PlotterCollection:
     def __init__(self, plotting_params, run_params, data_params):
@@ -39,7 +42,7 @@ class PlotterCollection:
         plt.rcParams["ytick.minor.size"] = 3.0
         plt.rcParams["figure.autolayout"] = True
         self.yscale = plotting_params['y_scale']
-        plt.rcParams['savefig.dpi'] = 300
+        plt.rcParams['savefig.dpi'] = 600
         self.imdir = plotting_params['image_directory']
         self.plotting_params = plotting_params
         self.run_params = run_params
@@ -47,6 +50,33 @@ class PlotterCollection:
         if not os.path.isdir(self.imdir):
             os.mkdir(self.imdir)
         return
+
+    def _time_lab(self):
+        """
+        Determines time scaling to use and labelling
+
+        Returns
+        -------
+        scaling_factor : float
+            Factor to scale time in (s) by
+        xlab : str
+            Time label to use
+        """
+        tf = self.run_params['final_time']
+        second = 1
+        millisecond = 1e-3 * second
+        minute = 60 * second
+        hour = 60 * minute
+        day = 24 * hour
+        year = 365.25 * day
+        time_list = [year, day, hour, minute, second, millisecond]
+        label_list = ['yr', 'd', 'hr', 'min', 's', 'ms']
+        for ti, time in enumerate(time_list):
+            if time > tf:
+                continue
+            xlab = f'Time [{label_list[ti]}]'
+            scaling_factor = 1 / time
+            return scaling_factor, xlab
     
     def parasitic_plot(self, parasitic_data):
         """
@@ -60,23 +90,107 @@ class PlotterCollection:
         
         """
         parasitic_use_data = parasitic_data['parasitic']
+        scale_fac, xlab = self._time_lab()
         for nuclide_index in range(np.shape(parasitic_use_data)[1]):
             for method_i in range(len(parasitic_data['parasitic'])):
                 method_name = parasitic_data['labs'][method_i]
-                x = self.run_params['times']
+                x = self.run_params['times'] * scale_fac
                 y = parasitic_use_data[method_i][nuclide_index]
                 lab = self.data_params['tracked_nucs'][nuclide_index]
                 plt.plot(x, y, label=f'{method_name} {lab} Captures')
             plt.yscale(self.yscale)
-            plt.xlabel('Time [s]')
-            plt.ylabel('Concentration [atoms/cc]')
+            plt.xlabel(xlab)
+            plt.ylabel('Absorptions [atoms/cc]')
             plt.legend()
             plt.tight_layout()
             plt.savefig(f'{self.imdir}parasitic_absorption_{nuclide_index}.png')
             plt.close()
         return
     
-    def plot_time(self, data_dict, spatial_eval_node=None):
+    def _time_plot_helper(self, data_dict, nuclide_i, ending,
+                          spatial_eval_node):
+        plt.xlabel(data_dict['xlab'])
+        plt.ylabel(data_dict['ylab'])
+        plt.yscale(self.yscale)
+        plt.legend()
+        try:
+            plot_main_name = f'{self.imdir}{data_dict['savename']}'
+            plt.savefig(f'{plot_main_name}_{nuclide_i}_{ending}.png')
+        except UnboundLocalError:
+            pos = spatial_eval_node
+            print(f'No data for {nuclide_i} figure at spatial index {pos}')
+        plt.close()
+        return
+    
+    def _data_collector(self, data_dict, data_str, spatial_eval_node,
+                        ending, nuclide_i, print_diffs, pcnt_diff_bool=False,
+                        print_data=False):
+        initial_data = True
+        if type(spatial_eval_node) == type(None):
+            for i, x in enumerate(data_dict['xs']):
+                try:
+                    lab = data_dict[f'lab_method{i}_nuc{nuclide_i}']
+                    y = data_dict[f'{data_str}_avg_y_method{i}_nuc{nuclide_i}']
+
+                    self.plot_dict['times'] = data_dict['xs'][i]
+                    self.plot_dict['data'] = data_dict[f'{data_str}_avg_y_method{i}_nuc{nuclide_i}']
+                    self.plot_obj.collect_data(data_dict, lab)
+
+                except KeyError:
+                    continue
+                ending = ending
+                plt.plot(x, y, label=lab)
+                if print_data:
+                    with open('data.txt', 'a') as f:
+                        f.write(f'{lab = }\n')
+                        f.write(f'{x = }\n')
+                        f.write(f'{y = }\n\n')
+                if initial_data:
+                    base_y = y
+                    base_lab = lab
+                    initial_data = False
+                if i != 0 and print_diffs:
+                    pcnt_diff = (y[-1] - base_y[-1]) / (y[-1]) * 100
+                    print(f'% diff {data_str} {lab}-{base_lab}: {pcnt_diff:.3E}%')
+                if print_diffs:
+                    print(f'    Final val {lab}: {y[-1]:.3E}')
+            self._time_plot_helper(data_dict, nuclide_i, ending,
+                                    spatial_eval_node)
+            if pcnt_diff_bool:
+                for i, x in enumerate(data_dict['xs']):
+                    if i == 0:
+                        continue
+                    lab = f'{base_lab} - {data_dict[f"lab_method{i}_nuc{nuclide_i}"]}'
+                    y = data_dict[f'{data_str}_avg_y_method{i}_nuc{nuclide_i}']
+                    pcnt_diffs = ((base_y - y) / (base_y) * 100)
+                    pcnt_diffs[np.isnan(pcnt_diffs)] = 0
+                    plt.plot(x, pcnt_diffs, label=lab)
+                plt.xlabel(data_dict['xlab'])
+                plt.ylabel('Percent Difference [%]')
+                plt.yscale(self.yscale)
+                plt.legend()
+                plot_main_name = f'{self.imdir}{data_dict['savename']}'
+                plt.savefig(f'{plot_main_name}_{nuclide_i}_pcntdiff.png')
+                plt.close()
+
+        else:
+            for i, x in enumerate(data_dict['xs']):
+                if type(spatial_eval_node) != type(None):
+                    try:
+                        lab = data_dict[f'lab_method{i}_nuc{nuclide_i}']
+                        y = data_dict['ys'][i][:, spatial_eval_node, nuclide_i]
+                    except IndexError:
+                        continue
+                    ending = f'spat{spatial_eval_node}'
+                    plt.plot(x, y, label=lab)
+            self._time_plot_helper(data_dict, nuclide_i, ending,
+                                spatial_eval_node)
+        return
+
+
+
+
+    def plot_time(self, data_dict, spatial_eval_node=None, print_diffs=True):
         """
         Plots average data over time where `data_dict` contains all
          necessary information
@@ -94,34 +208,50 @@ class PlotterCollection:
             num_nucs.append(np.shape(data_dict['ys'][i])[2])
         num_nucs = max(num_nucs)
         for nuclide_i in range(num_nucs):
+            if type(spatial_eval_node) == type(None):
+                self._data_collector(data_dict, 'spat', None,
+                                    'avg', nuclide_i, print_diffs,
+                                    pcnt_diff_bool=True, print_data=False)
+                self._data_collector(data_dict, 'in', None,
+                                    'in', nuclide_i, print_diffs)
+                self._data_collector(data_dict, 'ex', None,
+                                    'ex', nuclide_i, print_diffs)
+            if type(spatial_eval_node) != type(None):
+                self._data_collector(data_dict, None, spatial_eval_node,
+                                    None, nuclide_i, print_diffs)
+        return
+    
+    def plot_space(self, data_dict):
+        num_nucs = []
+        for i, x in enumerate(data_dict['xs']):
+            num_nucs.append(np.shape(data_dict['ys'][i])[2])
+        num_nucs = max(num_nucs)
+
+        for nuclide_i in range(num_nucs):
             for i, x in enumerate(data_dict['xs']):
-                if type(spatial_eval_node) == type(None):
-                    try:
-                        y = data_dict[f'spat_avg_y_method{i}_nuc{nuclide_i}']
-                    except KeyError:
-                        continue
-                    ending = 'avg'
-                else:
-                    try:
-                        y = data_dict['ys'][i][:, spatial_eval_node, nuclide_i]
-                    except IndexError:
-                        continue
-                    ending = f'spat{spatial_eval_node}'
+                try:
+                    y = data_dict['ys'][i][-1, :, nuclide_i]
+                except KeyError:
+                    continue
                 lab = data_dict[f'lab_method{i}_nuc{nuclide_i}']
-                plt.plot(x, y, label=lab)
-            plt.xlabel(data_dict['xlab'])
-            plt.ylabel(data_dict['ylab'])
-            plt.yscale(self.yscale)
+
+                plt.plot(self.run_params['positions'], y, label=lab,
+                        marker='.')
             plt.legend()
-            try:
-                plot_main_name = f'{self.imdir}{data_dict['savename']}'
-                plt.savefig(f'{plot_main_name}_{nuclide_i}_{ending}.png')
-            except UnboundLocalError:
-                pos = spatial_eval_node
-                print(f'No data for {nuclide_i} figure at spatial index {pos}')
+            plt.xlabel('Space [cm]')
+            max_conc = np.max(data_dict['ys'][i][:, :, nuclide_i])
+            plt.vlines(self.run_params['core_outlet'], 0, 1e1 * max_conc,
+                        color='black')
+            plt.ylabel('Concentration [at/cc]')
+            plt.ylim((1e-5 * max_conc, 1e1 * max_conc))
+            plt.yscale(self.yscale)
+            
+
+            plt.savefig(f'{self.imdir}final_time_{nuclide_i}.png')
             plt.close()
         return
 
+    
     def gif_generate(self, data_dict):
         """
         Plots spatial conc over time where `data_dict` contains all
@@ -176,7 +306,20 @@ class PlotterCollection:
         print(f'Gif creation took: {round(time() - start_time, 3)} s')
         return
     
-    def plot_gen(self, data_dict, spatial_eval_positions=[]):
+    def write_csv(self, data_dict):
+        num_nucs = []
+        for i, x in enumerate(data_dict['xs']):
+            num_nucs.append(np.shape(data_dict['ys'][i])[2])
+        num_nucs = max(num_nucs)
+        for nuclide_i in range(num_nucs):
+            for i, x in enumerate(data_dict['xs']):
+                df = pd.DataFrame(data_dict['ys'][i][:, :, nuclide_i])
+                df.insert(loc=0, column='Time [s]', value=self.run_params['times'])
+                df.to_csv(f'{self.imdir}data_{nuclide_i}.csv', index=False)
+        return
+    
+    def plot_gen(self, data_dict, spatial_eval_positions=[],
+                 time_eval_positions=[]):
         """
         Generates various plots based on plotting parameters
 
@@ -191,7 +334,13 @@ class PlotterCollection:
         """
         if not self.plotting_params['plotting']:
             return
+        self.plot_obj = PlotHolder()
+        self.plot_dict = {}
+        self.plot_dict['data'] = {}
+        self.plot_obj.plot_data()
         self.plot_time(data_dict)
+        #self.write_csv(data_dict)
+        self.plot_space(data_dict)
         for pos in spatial_eval_positions:
             self.plot_time(data_dict, pos) 
         if self.plotting_params['gif']:
