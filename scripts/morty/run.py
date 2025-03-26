9d6c1aab3b4f43befd39ccb091bcaae8b0146540
Luke Seifert <LukeSeifert23@gmail.com>
2025-03-26 13:43:20 -0500

    Add timing to run script

--- scripts/morty/run.py
+++ scripts/morty/run.py
@@ -3,6 +3,7 @@ import data
 import analysis
 import plotter
 import numpy as np
+from time import time
 
 
 def check_data(run_params, allowed_params):
@@ -31,6 +32,7 @@ def check_data(run_params, allowed_params):
 
 
 if __name__ == '__main__':
+    start = time()
     np.set_printoptions(threshold=np.inf)
     plotting_params = {}
     plotting_params['plotting'] = True
@@ -38,12 +40,13 @@ if __name__ == '__main__':
     plotting_params['gif'] = False
     plotting_params['parasitic_absorption'] = False
     plotting_params['image_directory'] = './images/'
-    plotting_params['msre'] = False
+    plotting_params['msre'] = True
+    plotting_params['surf_plot'] = True
 
     analysis_params = {}
-    analysis_params['test_run'] = True
+    analysis_params['test_run'] = False
     test_name = 'Scaled Flux'
-    analysis_params['PDE_ODE_compare'] = False
+    analysis_params['PDE_ODE_compare'] = True
     analysis_params['nuclide_refinement'] = False
     analysis_params['spatial_refinement'] = False
 
@@ -54,12 +57,12 @@ if __name__ == '__main__':
     run_params['neutron_energy'] = 0.0253
     run_params['chain_path'] = '../../data/chain_endfb71_pwr.xml'
     run_params['fissile_nuclide'] = 'U235'
-    run_params['target_element'] = 'Sb' #'Nb'
+    run_params['target_element'] = 'Xe' #'Nb'
     run_params['target_isobar'] = '135' #'95'
     run_params['spacenodes'] = 200
-    run_params['num_nuclides'] = 2
+    run_params['num_nuclides'] = 5
     run_params['data_gen_option'] = 'openmc'
-    run_params['final_time'] = 3600 #1.25*24*3600 + 100000 #  + 100000 #56340000 #1.25*24*3600 + 100_000 #29_210_400 #1.25 * 24 * 3600 #5
+    run_params['final_time'] = 1.25*24*3600 + 100000 #56340000 #1.25*24*3600 + 100000 #  + 100000 #56340000 #1.25*24*3600 + 100_000 #29_210_400 #1.25 * 24 * 3600 #5
     run_params['solver_method'] = 'PDE'
     run_params['flux'] = 8.4e12 # 2.9e12 #1.61e13 #6e12
     run_params['frac_in'] = 0.272
@@ -119,8 +122,7 @@ if __name__ == '__main__':
     if analysis_params['PDE_ODE_compare']:
         print('-' * 50)
         data_dict = analyzer.ode_pde_compare()
-        plotter_tool.plot_gen(data_dict, spatial_eval_positions=[0, -1,
-                                                                 int(run_params['spacenodes']*run_params['frac_in'])])
+        plotter_tool.plot_gen(data_dict, spatial_eval_positions=[0, -1, int(run_params['spacenodes']*run_params['frac_in'])], surf_opt=plotting_params['surf_plot'])
 
     if analysis_params['nuclide_refinement']:
         print('-' * 50)
@@ -132,3 +134,5 @@ if __name__ == '__main__':
         spatial_nodes = [5, 10, 100, 200, 500, 1000]
         data_dict = analyzer.spatial_refinement(spatial_nodes)
         plotter_tool.plot_gen(data_dict, spatial_eval_positions=[])
+
+    print(f'Took {round(time() - start, 1)} seconds')
