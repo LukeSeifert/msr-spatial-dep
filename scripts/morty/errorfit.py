8bee101b2f4c041bf0037dfaff7d2267f28cc7f4
Luke Seifert <seifert5@illinois.edu>
2025-04-06 12:41:40 -0500

    Cleaned up chemical error fitting

--- scripts/morty/errorfit.py
+++ scripts/morty/errorfit.py
@@ -19,18 +19,26 @@ plt.rcParams["ytick.minor.size"] = 3.0
 plt.rcParams["figure.autolayout"] = True
 plt.rcParams['savefig.dpi'] = 600
 
+ss_log = False
 
-
-xs = [5, 1, 0.5, 0.2, 0.1, 0.05, 0.03, 0.01, 0.0]
-peak = [93.37, 72.1, 53.6, 27.5, 14.7, 7.5, 4.57, 1.5, 0.0033]
-ss = [93.37, 72.1, 53.4, 23.2, 7.75, 2.0, 0.72, 0.085, 9.4e-6]
+#xs = [5, 1, 0.5, 0.2, 0.1, 0.05, 0.03, 0.01, 0.0]
+#peak = [93.37, 72.1, 53.6, 27.5, 14.7, 7.5, 4.57, 1.5, 0.0033]
+#ss = [93.37, 72.1, 53.4, 23.2, 7.75, 2.0, 0.72, 0.085, 9.4e-6]
+xs = [0.05, 0.04, 0.03, 0.02, 0.01, 0.0]
+peak = [7.5, 6.07, 4.57, 3.06, 1.5, 0.0033]
+ss = [2.0, 1.29, 0.72, 0.32, 0.085, 9.4e-6]
 #peak = [15.76, 8.64, 4.92, 3.02, 1.10]
 #ss = [4.10, 2.24, 1.71, 1.56, 0.80]
 
 coef = np.polyfit(xs,peak,1)
 poly1d_peak = np.poly1d(coef) 
-coef = np.polyfit(xs,ss,1)
-poly1d_ss = np.poly1d(coef) 
+if not ss_log:
+    coef = np.polyfit(xs,ss,2)
+    poly1d_ss = np.poly1d(coef) 
+else:
+    coef = np.polyfit(xs,np.log(ss),1)
+    poly1d_ss = lambda xs: [np.exp(coef[1]) * np.exp(coef[0] * x) for x in xs]
+
 # poly1d_fn is now a function which takes in x and returns an estimate for y
 print(f'{poly1d_peak = }')
 print(f'{poly1d_ss = }')
