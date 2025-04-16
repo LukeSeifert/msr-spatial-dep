a642b03081487977cdaf5d0494de2bf18eade93a
Luke Seifert <seifert5@illinois.edu>
2025-04-16 07:13:36 -0500

    add flow rate to error fit

--- scripts/morty/errorfit.py
+++ scripts/morty/errorfit.py
@@ -20,18 +20,38 @@ plt.rcParams["figure.autolayout"] = True
 plt.rcParams['savefig.dpi'] = 600
 
 ss_log = False
+peak_log = False
 
-#xs = [5, 1, 0.5, 0.2, 0.1, 0.05, 0.03, 0.01, 0.0]
-#peak = [93.37, 72.1, 53.6, 27.5, 14.7, 7.5, 4.57, 1.5, 0.0033]
-#ss = [93.37, 72.1, 53.4, 23.2, 7.75, 2.0, 0.72, 0.085, 9.4e-6]
-xs = [0.05, 0.04, 0.03, 0.02, 0.01, 0.0]
-peak = [7.5, 6.07, 4.57, 3.06, 1.5, 0.0033]
-ss = [2.0, 1.29, 0.72, 0.32, 0.085, 9.4e-6]
-#peak = [15.76, 8.64, 4.92, 3.02, 1.10]
-#ss = [4.10, 2.24, 1.71, 1.56, 0.80]
+chem = False
+flow_scaled = True
+flow_unscaled = False
 
-coef = np.polyfit(xs,peak,1)
-poly1d_peak = np.poly1d(coef) 
+separate_marker_plot = False
+
+length = 608.06
+
+if chem:
+    xs = [0.05, 0.04, 0.03, 0.02, 0.01, 0.0]
+    peak = [7.5, 6.07, 4.57, 3.06, 1.5, 0.0033]
+    ss = [2.0, 1.29, 0.72, 0.32, 0.085, 9.4e-6]
+elif flow_scaled:
+    xs = [21.7e1, 21.7, 21.7e-1, 21.7e-2, 21.7e-3, 21.7e-4, 21.7e-5]
+    peak = [0.00036, 0.003, 0.027, 0.3, 2.3, 15.5, 49]
+    ss = [1.6e-7, 7.5e-6, 0.002, 0.06, 0.78, 0.43, 49]
+    xs = [length/x for x in xs]
+#elif flow_unscaled:
+#    xs = [21.7e1, 21.7, 21.7e-1, 21.7e-2, 21.7e-3, 21.7e-4, 21.7e-5]
+#    peak = [267, 267, 267, 268, 268, 267, 268]
+#    ss = [262, 218, 130, 130, 128, 131, 243]
+xfine = np.linspace(min(xs), max(xs), 100)
+
+if not peak_log:
+    coef = np.polyfit(xs,peak,1)
+    poly1d_peak = np.poly1d(coef) 
+else:
+    coef = np.polyfit(xs,np.log(peak),1)
+    poly1d_peak = lambda xs: [np.exp(coef[1]) * np.exp(coef[0] * x) for x in xs]
+
 if not ss_log:
     coef = np.polyfit(xs,ss,2)
     poly1d_ss = np.poly1d(coef) 
@@ -45,14 +65,30 @@ print(f'{poly1d_ss = }')
 
 #plt.plot(x,y, 'yo', x, poly1d_fn(x), '--k') #'--k'=black dashed line, 'yo' = yellow circle marker
 
+if separate_marker_plot:
+    plt.plot(xs, peak, marker='.', linestyle='', color='blue')
+    plt.plot(xfine, poly1d_peak(xfine), label='Peak Fit', color='blue', linestyle='--')
+    plt.plot(xs, ss, marker='x', linestyle='', color='orange')
+    plt.plot(xfine, poly1d_ss(xfine), label='Final Fit', color='orange', linestyle='-.')
+else:
+    plt.plot(xs, peak, marker='.', linestyle='--', color='blue', label='Peak')
+    plt.plot(xs, ss, marker='x', linestyle='-.', color='orange', label='Equilibrium')
 
-plt.plot(xs, peak, marker='.', linestyle='', color='blue')
-plt.plot(xs, poly1d_peak(xs), label='Peak Fit', color='blue', linestyle='--')
-plt.plot(xs, ss, marker='x', linestyle='', color='orange')
-plt.plot(xs, poly1d_ss(xs), label='Final Fit', color='orange', linestyle='--')
 plt.ylabel('Error [%]')
 plt.legend()
-plt.xlabel('Removal Rate [$s^{-1}$]')
+if chem:
+    plt.xlabel('Removal Rate [1/s]')
+    save_name = 'removal_err'
+else:
+    #plt.xlabel('Flow Rate [cm/s]')
+    plt.xlabel('Residence Time [s]')
+    plt.xscale('log')
+    plt.yscale('log')
+if flow_scaled:
+    save_name = 'flow_scaled_err'
+#elif flow_unscaled:
+#    save_name = 'flow_unscaled_err'
+
 plt.tight_layout()
-plt.savefig('removal_err.png')
+plt.savefig(f'{save_name}.png')
 plt.close()
