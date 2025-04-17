84b6f2feb75d830e3e74ac6f6d5b1dd19405dbb5
Luke Seifert <seifert5@illinois.edu>
2025-04-17 12:07:34 -0500

    remove debug prints

--- scripts/morty/data.py
+++ scripts/morty/data.py
@@ -432,9 +432,7 @@ class DataHandler:
             lams[i] = np.log(2) / half_life_data[i]
             try:
                 repr_val[i] = self.run_params['reprocessing'][tracked_element]
-                print(f'{tracked_element} has non-zero repr rate')
             except KeyError:
-                print(f'Repr of {nuc_names[i]} = 0')
                 repr_val[i] = 0
             FYs[i] = fiss_macro_xs * yield_data[i] * self.flux
             loss_rates[i] = net_xs_data[i] * self.flux
